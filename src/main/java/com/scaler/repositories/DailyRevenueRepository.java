fb3b588368fde63351115594089d8e1c49a7cb6c
Mithun <mithuncm@gmail.com>
2024-09-23 01:32:24 +0530

    WIP v1.3

--- src/main/java/com/scaler/repositories/DailyRevenueRepository.java
+++ src/main/java/com/scaler/repositories/DailyRevenueRepository.java
@@ -1,14 +1,11 @@
 package com.scaler.repositories;
 
 import com.scaler.models.DailyRevenue;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
-@Repository
-public interface DailyRevenueRepository extends JpaRepository<DailyRevenue, Integer> {
+public interface DailyRevenueRepository {
     public DailyRevenue save(DailyRevenue dailyRevenue);
 
     public List<DailyRevenue> getDailyRevenueBetweenDates(Date startDate, Date endDate);
--- src/main/java/com/scaler/repositories/UserRepository.java
+++ src/main/java/com/scaler/repositories/UserRepository.java
@@ -1,13 +1,10 @@
 package com.scaler.repositories;
 
 import com.scaler.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository {
     Optional<User> findById(long id);
 
     User save(User user);
