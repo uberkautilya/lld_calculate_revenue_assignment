0c3076d9742bc2ab1136201316aa2a1e57e69d2b
Mithun <mithuncm@gmail.com>
2024-09-23 01:46:12 +0530

    WIP v1.4

--- src/main/java/com/scaler/repositories/DailyRevenueRepositoryImpl.java
+++ src/main/java/com/scaler/repositories/DailyRevenueRepositoryImpl.java
@@ -11,11 +11,22 @@ import java.util.stream.Collectors;
 @Repository
 public class DailyRevenueRepositoryImpl implements DailyRevenueRepository {
     List<DailyRevenue> dailyRevenues = new ArrayList<>();
+
     @Override
     public DailyRevenue save(DailyRevenue dailyRevenue) {
-        return dailyRevenues.stream()
+        DailyRevenue existingRevenue = dailyRevenues.stream()
                 .filter(revenue -> revenue.getId() == dailyRevenue.getId())
                 .findFirst().orElse(null);
+        if(existingRevenue != null){
+            existingRevenue.setDate(dailyRevenue.getDate());
+            existingRevenue.setRevenueFromFoodSales(dailyRevenue.getRevenueFromFoodSales());
+            existingRevenue.setTotalGst(dailyRevenue.getTotalGst());
+            existingRevenue.setTotalServiceCharge(dailyRevenue.getTotalServiceCharge());
+            return existingRevenue;
+        } else {
+            dailyRevenues.add(dailyRevenue);
+            return dailyRevenue;
+        }
     }
 
     @Override
